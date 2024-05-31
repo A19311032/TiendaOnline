ce8ea2e51193c700e7efcf9bb49018a521e1660d
MARTIN ALEJANDRO MARMOLEJO CASTILLO <a20311044@uthermosillo.edu.mx>
2024-05-30 19:41:36 -0700

    se creo modulo provedores

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -24,7 +24,7 @@
                     <form method="POST" action="{{ route('perfil.update') }}">
                         @csrf
                         @method('POST')
-
+<!--hola-->
                         <div class="form-group">
                             <label for="phone">Teléfono</label>
                             <input type="text" name="phone" value="{{ $user->phone }}" class="form-control">
