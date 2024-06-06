267db640cf819e441e433a63fd976a4685137e7f
a19311032 <a19311032@uthermosillo.edu.mx>
2024-06-05 19:55:08 -0700

    Correcion en migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,10 +20,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('phone');
-            $table->string('brand');
-            $table->string('social');
-            $table->integer('level');
+            $table->string('phone')->nullable();
+            $table->string('brand')->nullable();
+            $table->string('social')->nullable();
+            $table->integer('level')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
