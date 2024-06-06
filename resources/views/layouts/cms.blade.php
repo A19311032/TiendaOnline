da2228a061765be096c29480aee76e7f44ac6063
a19311032 <a19311032@uthermosillo.edu.mx>
2024-06-05 20:18:04 -0700

    Prueba CodeSpace

--- resources/views/layouts/cms.blade.php
+++ resources/views/layouts/cms.blade.php
@@ -291,7 +291,7 @@
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span
                                     class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
-                                <img class="img-profile rounded-circle" src="{{asset('img/undraw_profile.svg')}}">
+                                <img class="img-profile rounded-circle" src="{{asset('img/hasbulla.png')}}">
                             </a>
 
                             <!-- Dropdown - User Information -->
